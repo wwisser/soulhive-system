4044b6f7f7f73e054ff9db1f4ee14385399f1d70
Wende2k <wisserwendelin@gmail.com>
2018-11-25 21:10:18 +0100

    Added TeamSpeak command

---
+++ src/main/java/de/soulhive/system/command/impl/CommandTeamspeak.java
@@ -0,0 +1,15 @@
+package de.soulhive.system.command.impl;
+
+import de.soulhive.system.command.CommandExecutorWrapper;
+import de.soulhive.system.command.exception.CommandException;
+import de.soulhive.system.setting.Settings;
+import org.bukkit.command.CommandSender;
+
+public class CommandTeamspeak extends CommandExecutorWrapper {
+
+    @Override
+    public void process(CommandSender sender, String label, String[] args) throws CommandException {
+        sender.sendMessage(Settings.PREFIX + "Unsere TeamSpeak-IP: §fts.soulhive.de");
+    }
+
+}
