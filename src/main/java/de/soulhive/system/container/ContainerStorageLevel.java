682612c07ed3836650cc7b48a9f2ae0acceadba2
Wende2k <wisserwendelin@gmail.com>
2018-12-08 16:27:26 +0100

    Created ContainerStorageLevel (STORED, NEW)

---
+++ src/main/java/de/soulhive/system/container/ContainerStorageLevel.java
@@ -0,0 +1,14 @@
+package de.soulhive.system.container;
+
+public enum ContainerStorageLevel {
+
+    /**
+     * Generates a container once and stores it as field.
+     */
+    STORED,
+    /**
+     * Generates a container every time again.
+     */
+    NEW
+
+}
