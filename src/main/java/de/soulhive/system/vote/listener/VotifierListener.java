cd6aa9ee935d85c61085fa7d4cf80a0565d2a287
Wende2k <wisserwendelin@gmail.com>
2019-02-08 13:28:56 +0100

    Added user vote count into VotifierListener

--- src/main/java/de/soulhive/system/vote/listener/VotifierListener.java
+++ src/main/java/de/soulhive/system/vote/listener/VotifierListener.java
@@ -2,17 +2,23 @@ package de.soulhive.system.vote.listener;
 
 import com.vexsoftware.votifier.model.Vote;
 import com.vexsoftware.votifier.model.VotifierEvent;
+import de.soulhive.system.SoulHive;
+import de.soulhive.system.user.UserService;
 import de.soulhive.system.vote.VoteService;
-import lombok.AllArgsConstructor;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
-@AllArgsConstructor
 public class VotifierListener implements Listener {
 
     private VoteService voteService;
+    private UserService userService;
+
+    public VotifierListener(final VoteService voteService) {
+        this.voteService = voteService;
+        this.userService = SoulHive.getServiceManager().getService(UserService.class);
+    }
 
     @EventHandler
     public void onVote(VotifierEvent event) {
@@ -21,6 +27,7 @@ public class VotifierListener implements Listener {
 
         if (player != null && player.isOnline()) {
             this.voteService.handleVote(player);
+            this.userService.getUser(player).addVote();
         }
     }
 
