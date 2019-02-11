4cd82bda94f378bad6004fc6e99dce7d053b7e17
Wende2k <wisserwendelin@gmail.com>
2019-02-11 10:10:11 +0100

    Added event priority into ShowhealServide

--- src/main/java/de/soulhive/system/service/micro/ShowhealService.java
+++ src/main/java/de/soulhive/system/service/micro/ShowhealService.java
@@ -6,6 +6,7 @@ import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Projectile;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 
@@ -17,7 +18,7 @@ public class ShowhealService extends Service implements Listener {
         super.registerListener(this);
     }
 
-    @EventHandler
+    @EventHandler(ignoreCancelled = true, priority = EventPriority.HIGH)
     public void onEntityDamageByEntity(final EntityDamageByEntityEvent event) {
         if (event.getDamager() instanceof Projectile) {
             final Projectile bullet = (Projectile) event.getDamager();
