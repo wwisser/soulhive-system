b59a02528002171e8bcfc4076cfa0502fec1ec72
Wende2k <wisserwendelin@gmail.com>
2019-02-16 19:05:26 +0100

    Added -u parameter for CommandSpectate

--- src/main/java/de/soulhive/system/command/impl/admin/CommandSpectate.java
+++ src/main/java/de/soulhive/system/command/impl/admin/CommandSpectate.java
@@ -3,6 +3,8 @@ package de.soulhive.system.command.impl.admin;
 import de.soulhive.system.command.CommandExecutorWrapper;
 import de.soulhive.system.command.exception.CommandException;
 import de.soulhive.system.command.util.ValidateCommand;
+import de.soulhive.system.setting.Settings;
+import org.bukkit.GameMode;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
@@ -16,6 +18,14 @@ public class CommandSpectate extends CommandExecutorWrapper {
         ValidateCommand.minArgs(1, args, USAGE);
 
         Player player = ValidateCommand.onlyPlayer(sender);
+
+        if (args[0].equalsIgnoreCase("-u")) {
+            player.teleport(Settings.LOCATION_SPAWN);
+            player.setGameMode(GameMode.SURVIVAL);
+            player.chat("/vanish");
+            return;
+        }
+
         Player target = ValidateCommand.target(args[0], player);
 
         player.chat("/vanish");
