d50c3c5d553ff2fe91b766b0bc081ca399f85928
Wende2k <wisserwendelin@gmail.com>
2019-03-30 23:09:22 +0100

    Fixed npc spawn bug

--- src/main/java/de/soulhive/system/listener/impl/guard/EntitySpawnListener.java
+++ src/main/java/de/soulhive/system/listener/impl/guard/EntitySpawnListener.java
@@ -1,7 +1,10 @@
 package de.soulhive.system.listener.impl.guard;
 
+import de.soulhive.system.SoulHive;
+import de.soulhive.system.npc.NpcService;
 import de.soulhive.system.setting.Settings;
 import org.bukkit.Location;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -11,11 +14,14 @@ public class EntitySpawnListener implements Listener {
 
     @EventHandler
     public void onEntitySpawn(EntitySpawnEvent event) {
+        Entity entity = event.getEntity();
         Location location = event.getLocation();
+        NpcService npcService = SoulHive.getServiceManager().getService(NpcService.class);
 
         if (event.getEntityType() != EntityType.PLAYER
             && location.getWorld().equals(Settings.WORLD_MAIN)
-            && location.getBlockY() > Settings.SPAWN_HEIGHT) {
+            && location.getBlockY() > Settings.SPAWN_HEIGHT
+            & !npcService.isNpc(entity)) {
             event.setCancelled(true);
         }
     }
