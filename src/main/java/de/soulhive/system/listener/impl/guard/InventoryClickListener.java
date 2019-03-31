e2900d7528f82ef331aea02efcb84816f9129776
Wende2k <wisserwendelin@gmail.com>
2019-03-31 19:37:38 +0200

    Removed unused imports

--- src/main/java/de/soulhive/system/listener/impl/guard/InventoryClickListener.java
+++ src/main/java/de/soulhive/system/listener/impl/guard/InventoryClickListener.java
@@ -1,9 +1,6 @@
 package de.soulhive.system.listener.impl.guard;
 
-import org.bukkit.entity.HumanEntity;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.AnvilInventory;
